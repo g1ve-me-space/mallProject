92a194f4c42dc30031d332f1b9f0825eb98db753
Farcas Radu Stelian <radustefar@gmail.com>
2025-11-18 01:24:01 +0200

    all done. goon and sleep time

--- src/main/java/repository/StaffAssignmentRepository.java
+++ src/main/java/repository/StaffAssignmentRepository.java
@@ -1,10 +1,16 @@
 package repository;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import model.StaffAssignment;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 // Inherits all standard methods from our new InMemoryRepository
-public class StaffAssignmentRepository extends InMemoryRepository<StaffAssignment, String> {
-    // This body is intentionally left empty.
+public class StaffAssignmentRepository extends InFileRepository<StaffAssignment> {
+
+    public StaffAssignmentRepository() {
+        super("staffAssignment.json", new TypeReference<List<StaffAssignment>>() {});
+    }
 }
